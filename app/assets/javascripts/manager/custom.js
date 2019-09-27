97dfeabe3c864b3ce6e4813d470fa8f880d289e4
HieuCR7 <hieuhv97@gmail.com>
2019-09-27 14:28:50 +0700

    charts revenue by year, month, day and refactor code

--- app/assets/javascripts/manager/custom.js
+++ app/assets/javascripts/manager/custom.js
@@ -1,78 +1,23 @@
 $(document).ready(function() {
-  // var canvasMyChart = document.getElementById("myChart");
-  var newCharts;
-  $('.order-year').click(function(){
-    var year = $('.order-year').val();
-    if (year) {
-      $.ajax({
-        url: '/manager/charts/order_by_year',
-        type: 'GET',
-        dataType: 'JSON',
-        data: {
-          year: year
-        },
-        success: function(data) {
-          $('#myChart').remove();
-          $('.to_day_order').append('<canvas class="chart_by_year" id="myChart"></canvas>');
-          var keys = Object.keys(data);
-          var values = Object.values(data);
-          var ctx = $('#myChart');
-          var chartByYear = new Chart(ctx, {
-            type: 'bar',
-            data: {
-              labels: keys,
-              datasets: [
-                {
-                  label: "Number oder",
-                  backgroundColor: "#A52A2A",
-                  data: values
-                }
-              ]
-            },
-            options: {
-              title: {
-                display: true,
-                text: 'Number Order By Year',
-                fontSize: 15
-              },
-              legend: {
-                display: true,
-                labels: {
-                  fontColor: '#A52A2A'
-                }
-              },
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
-            }
-          });
-          newCharts = chartByYear;
-        }
-      });
-    } else {
-      $('#myChart').empty();
-    }
-  });
+  var yearChart;
+  var monthChart;
+  var totalChart;
 
-  $(document).on('click', '#myChart', function(evt){
-    var activePoints = newCharts.getElementsAtEvent(evt);
-    var year = $('.order-year').val();
-    if (activePoints[0]) {
-      var chartData = activePoints[0]['_chart'].config.data;
-      var idx = activePoints[0]['_index'];
-      var label = chartData.labels[idx];
-      // var value = chartData.datasets[0].data[idx];
-      if(label) {
+  refreshPage();
+
+  // Charts Order by the month in year
+  $(document).on('click', '#myChart', function(event){
+    var activePoint = yearChart.getElementAtEvent(event)[0];
+    if (activePoint) {
+      var month = activePoint['_model'].label;
+      var year = activePoint['_model'].datasetLabel;
+      if(month) {
         $.ajax({
-          url: '/manager/charts/order_by_month',
+          url: '/manager/charts/revenue_in_month',
           type: 'GET',
           dataType: 'JSON',
           data: {
-            month: label,
+            month: month,
             year: year
           },
           success: function(data) {
@@ -80,41 +25,8 @@ $(document).ready(function() {
             $('.to_month_order').append('<canvas id="myChartByMonth"></canvas>');
             var keys = Object.keys(data);
             var values = Object.values(data);
-            var canvasMyChart = document.getElementById("myChartByMonth");
-            var ctx = canvasMyChart.getContext("2d");
-            var chartByYear = new Chart(ctx, {
-              type: 'line',
-              data: {
-                labels : keys,
-                datasets : [
-                  {
-                    label: "Number oder",
-                    backgroundColor: "#B0E0E6",
-                    data: values
-                  }
-                ]
-              },
-              options: {
-                title: {
-                  display: true,
-                  text: 'Number Order By Month',
-                  fontSize: 15
-                },
-                legend: {
-                  display: true,
-                  labels: {
-                    fontColor: '#A0522D'
-                  }
-                },
-                scales: {
-                  yAxes: [{
-                    ticks: {
-                      beginAtZero: true
-                    }
-                  }]
-                }
-              }
-            });
+            var ctx = $('#myChartByMonth');
+            monthChart = responseBarChart('horizontalBar', keys, values, month, 'Revenue In ' + month + ' ' + year, ctx, getRandomColor(), '#008080', '#3e95cd', 'Currency($)', 'Day' );
           }
         });
       } else {
@@ -122,4 +34,277 @@ $(document).ready(function() {
       }
     }
   });
+
+  //Charts compare between 2 year
+  var arr = [(new Date).getFullYear().toString()];
+  $('.year-1').click(function() {
+    if (arr.includes($(this).val()) != true) {
+      var year = $(this).val();
+      arr.push(year);
+    }
+    if ( year) {
+      $.ajax({
+        url: '/manager/charts/revenue_in_year',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+          year: year,
+        },
+        success: function(data) {
+          var values = Object.values(data);
+          dataset = {
+            label: year,
+            backgroundColor: getRandomColor(),
+            data: values,
+            borderColor: '#fff',
+            fill: false
+          };
+          addBarData(yearChart, dataset);
+        }
+      });
+    } else {
+      return false;
+    }
+  });
+
+  // Charts compare between the year
+  $('.year_2').click(function() {
+    var year_1 = $('.year_1').val();
+    var year_2 = $('.year_2').val();
+    if (year_1 && year_2) {
+      $.ajax({
+        url: '/manager/charts/compare_between_year',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+          year_1: year_1,
+          year_2: year_2
+        },
+        success: function(data) {
+          $('#pieChart').remove();
+          $('.pieChart').append('<canvas id="pieChart"></canvas>');
+          var keys = Object.keys(data);
+          var values = Object.values(data);
+          var ctx = $('#pieChart');
+          var color = []
+          keys.forEach(function(){
+            color.push(getRandomColor());
+          });
+          monthChart = responsePieChart(keys, values, 'Total Revenue The Year',ctx, color);
+        }
+      });
+    } else {
+      return false;
+    }
+  });
+
+  $('.year_1').click(function() {
+    var year_1 = $(this).val()
+    if (year_1) {
+      $.ajax({
+        url: '/manager/charts/select_to_year',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+          year_1: year_1
+        },
+        success: function(data) {
+          $('.year_2').empty();
+          for(var i in data) {
+            var option = '<option value=' + data[i] + '>' + data[i] + '</option>';
+            $('.year_2').append(option);
+          }
+        }
+      });
+    } else {
+      return false;
+    }
+  });
+
+  // auto load ajax after load page, displayed order by the year
+  function refreshPage() {
+    var year = (new Date).getFullYear().toString();
+    $.ajax({
+      url: '/manager/charts/revenue_in_year',
+      type: 'GET',
+      dataType: 'JSON',
+      data: {
+        year: year
+      },
+      success: function(data) {
+        $('#myChart').remove();
+        $('.to_day_order').append('<canvas class="chart_by_year" id="myChart"></canvas>');
+        var keys = Object.keys(data);
+        var values = Object.values(data);
+        var ctx = $('#myChart');
+        yearChart = responseBarChart('bar', keys, values, year, 'Revenue In ' + year, ctx, '#A52A2A', '#A52A2A', '', 'Month', 'Currency($)');
+      }
+    });
+
+    var year_1 = $('.year_1').val();
+    var year_2 = (new Date).getFullYear() - 4;
+    if (year_1 && year_2) {
+      $.ajax({
+        url: '/manager/charts/compare_between_year',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+          year_1: year_1,
+          year_2: year_2
+        },
+        success: function(data) {
+          $('#pieChart').remove();
+          $('.pieChart').append('<canvas id="pieChart"></canvas>');
+          var keys = Object.keys(data);
+          var values = Object.values(data);
+          var ctx = $('#pieChart');
+          var color = []
+          keys.forEach(function(){
+            color.push(getRandomColor());
+          });
+          monthChart = responsePieChart(keys, values, 'Total Revenue The Year',ctx, color);
+        }
+      });
+    } else {
+      return false;
+    };
+
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    var date = new Date();
+    var month = monthNames[date.getMonth()];
+    if (month) {
+      $.ajax({
+        url: '/manager/charts/revenue_in_month',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+          month: month,
+          year: year
+        },
+        success: function(data) {
+          $('#myChartByMonth').remove();
+          $('.to_month_order').append('<canvas id="myChartByMonth"></canvas>');
+          var keys = Object.keys(data);
+          var values = Object.values(data);
+          var ctx = $('#myChartByMonth');
+          monthChart = responseBarChart('horizontalBar', keys, values, month, 'Revenue In ' + month + ' ' + year, ctx, getRandomColor(), '#008080', '#3e95cd', 'Currency($)', 'Day' );
+        }
+      });
+    } else {
+      return false;
+    }
+  }
+
+  // function to callback Charts
+  function responseBarChart(type='', keys, values, label='', title='', ctx, backgroundColor='', fontColor='', borderColor='', xLabel, yLabel) {
+    var chartByYear = new Chart(ctx, {
+      type: type,
+      data: {
+        labels: keys,
+        datasets: [
+          {
+            label: label,
+            backgroundColor: backgroundColor,
+            data: values,
+            borderColor: borderColor,
+            fill: false
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: title,
+          fontSize: 15
+        },
+        legend: {
+          display: true,
+          labels: {
+            fontColor: fontColor
+          }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            scaleLabel: {
+              display: true,
+              labelString: yLabel
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            scaleLabel: {
+              display: true,
+              labelString: xLabel
+            }
+          }]
+        },
+        to: 1
+      }
+    });
+    return chartByYear;
+  };
+
+  function responsePieChart(key, value, title='', ctx, color) {
+    var chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: key,
+        datasets: [
+          {
+            label: key,
+            backgroundColor: color,
+            data: value,
+            borderColor: color,
+            fill: false
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: title,
+          fontSize: 15
+        },
+        legend: {
+          display: true,
+          labels: {
+            fontColor: color
+          }
+        }
+      }
+    });
+    return chart;
+  };
+
+  function addBarData(chart, dataset) {
+    chart.data.datasets.push(dataset);
+    chart.options.title.text += " and " + dataset.label;
+    chart.update();
+  };
+
+  function addPieChartData(chart, dataset) {
+    chart.data.labels.push(dataset.label);
+    chart.data.datasets[0].data.push(dataset.data);
+    chart.data.datasets[0].backgroundColor.push(dataset.color);
+    chart.data.datasets[0].borderColor.push(dataset.color);
+    chart.update();
+  };
+
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
 });
+
--- app/controllers/manager/charts_controller.rb
+++ app/controllers/manager/charts_controller.rb
@@ -2,21 +2,52 @@
 
 module Manager
   class ChartsController < BaseController
-    def index
-      @date = Order.all.map { |order| order.created_at.strftime("%Y") }.uniq
+    include MonthConcern
+    before_action :select_year, only: %i[index select_to_year]
+
+    def index; end
+
+    def revenue_in_year
+      revenue_default = revenue_month_default
+      revenue_in_year = Order.revenue_month_in_year params[:year]
+      render json: revenue_default.merge(revenue_in_year)
     end
 
-    def order_by_year
-      @condition_by_year = Order.by_year(params[:year])
-      @order_by_month = @condition_by_year.group_by_month(:created_at, format: "%b").count
-      render json: @order_by_month
+    def revenue_in_month
+      revenue_default = days_in_month
+      revenue_on_day = Order.revenue_day_in_month params[:month], params[:year]
+      render json: revenue_default.merge(revenue_on_day)
     end
 
-    def order_by_month
+    def compare_between_year
+      condition = condition_between_year params[:year_1], params[:year_2]
+      revenue_between_year = condition.group("YEAR(created_at)").sum("total_price")
+      render json: revenue_between_year
+    end
+
+    def select_to_year
+      year2 = @year.reject { |year| year == params[:year_1].to_i }
+      render json: year2
+    end
+
+    private
+
+    def select_year
+      @year = Order.pluck("YEAR(created_at)").uniq
+    end
+
+    def days_in_month
       @month = Settings.month[params[:month].to_sym]
-      @condition_by_month = Order.by_month_year(@month, params[:year])
-      @order_by_month = @condition_by_month.group_by_day(:created_at, format: "%a, %d").count
-      render json: @order_by_month
+      day_in_month = Time.days_in_month @month, params[:year].to_i
+      day_keys_default = (1..day_in_month).map(&:to_s)
+      day_values_default = day_keys_default.map { |_value| _value = 0 }
+      @day_hash_default = day_keys_default.zip(day_values_default).to_h
+    end
+
+    def condition_between_year(year1, year2)
+      return Order.created_between(year2, year1) if year1 >= year2
+
+      Order.created_between(year1, year2)
     end
   end
 end
